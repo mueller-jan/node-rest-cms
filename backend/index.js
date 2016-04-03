4fe8e220d34916756594e091646871f78077d402
Jan Mueller <jmueller02@stud.hs-bremen.de>
2016-04-03 14:41:13 +0200

    db setup

--- backend/index.js
+++ backend/index.js
@@ -17,68 +17,8 @@ restify.CORS.ALLOW_HEADERS.push('Access-Control-Allow-Origin');
 restify.CORS.ALLOW_HEADERS.push('x-access-token');
 server.use(restify.CORS());
 
-var mongoose = require('mongoose');
-var Page = require('./models/page');
-var Layout = require('./models/layout');
-var Menu = require('./models/menu');
 
-var layoutElements = [];
-
-Layout.count({}, function (err, result) {
-    if (err) throw err;
-    console.log("prefill database")
-    if (result === 0) {
-        var page = new Page({
-            slug: 'start',
-            date: new Date(),
-            content: '<p><span style="color: rgb(0, 0, 0);text-align: justify;float: none;background-color: rgb(255, 255, 255);"><span style="color: rgb(102, 102, 102);text-align: left;float: none;background-color: rgb(255, 255, 255);">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</span></span></p><p><span style="color: rgb(102, 102, 102);">In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</span></p><p><br/><img src="http://lorempixel.com/g/400/200/"/><br/></p><p><span style="color: rgb(102, 102, 102);"><br/></span></p><p><span style="color: rgb(102, 102, 102);">In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</span></p>',
-            title: 'Start'
-        });
-
-        page.save(function (err) {
-            if (err) throw err;
-        });
-
-        var page1 = new Page({
-            slug: 'page1',
-            date: new Date(),
-            title: 'Page1'
-        });
-
-        page1.save(function (err) {
-            if (err) throw err;
-        });
-
-        var menu = new Menu({
-            name: 'Default',
-            items: [page, page1]
-        });
-
-        menu.save(function (err) {
-            if (err) throw err;
-        });
-
-        layoutElements.push(new Layout({
-            name: 'toolbar',
-            content: 'NODE CMS',
-            items: [menu]
-        }));
-
-        layoutElements.push(new Layout({
-            name: 'sidebar-left'
-        }));
-
-        layoutElements.push(new Layout({
-            name: 'footer'
-        }));
-
-        for (var i = 0; i < layoutElements.length; i++)
-            layoutElements[i].save(function (err, l) {
-                if (err) throw err;
-            });
-    }
-});
-
+lib.db.setup();
 
 //setup unprotected routes (no authentication required)
 lib.helpers.setupRoutes(server, lib, false);
