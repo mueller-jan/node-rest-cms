76f90e285ff86b92b5facbccbcf02e75c1bcc596
mueller-jan <mueller.jan.87@gmail.com>
2016-04-11 17:53:38 +0200

    menu configuration via drag & drop

--- frontend/src/app/admin/menus/menus-edit.js
+++ frontend/src/app/admin/menus/menus-edit.js
@@ -21,36 +21,52 @@ angular.module('admin.menus-edit', [
         function EditMenusController($scope, $rootScope, $state, $stateParams, crudService, BASE_URL, SUCCESS_EVENTS) {
             $scope.pageId = $stateParams.id;
 
-            $scope.pagesToInclude = [];
-            $scope.categoriesToInclude = [];
+            $scope.itemsToInclude = [];
 
-            crudService.getPages().then(function (res) {
-                $scope.pages = res.data;
+            crudService.getPages('?type=page').then(function (res) {
+                $scope.pageItems = pagesToMenuItems(res.data);
             });
+
+            function pagesToMenuItems(pages) {
+                var pageItems = [];
+                for (var i = 0; i < pages.length; i++) {
+                    pageItems.push({
+                        'title': pages[i].title,
+                        'path': BASE_URL + '#/page/' + pages[i].slug
+                    })
+                }
+                return pageItems;
+            }
 
             crudService.getCategories().then(function (res) {
-                $scope.categories = res.data;
+                $scope.categoryItems = categoriesToMenuItems(res.data);
             });
+
+            function categoriesToMenuItems(categories) {
+                var catItems = [];
+                for (var i = 0; i < categories.length; i++) {
+                    catItems.push({
+                        'title': categories[i].title,
+                        'path': BASE_URL + '#/category/' + categories[i].slug
+                    })
+                }
+                return catItems;
+            }
+
 
             crudService.getMenu($stateParams.id).then(function (res) {
                 $scope.menu = res.data;
-                $scope.menu.items = [];
+                $scope.itemsToInclude = $scope.menu.items;
+                markObjectsAlreadyInMenu();
             });
 
+            $scope.deleteItem = function (item) {
+                $scope.itemsToInclude.splice(item, 1);
+            };
+
             $scope.submit = function () {
-                var i;
-                for (i = 0; i < $scope.pagesToInclude.length; i++) {
-                    $scope.menu.items.push({
-                        'title': $scope.pagesToInclude[i].title,
-                        'path': BASE_URL + '#/page/' + $scope.pagesToInclude[i].slug
-                    });
-                }
-                for (i = 0; i < $scope.categoriesToInclude.length; i++) {
-                    $scope.menu.items.push({
-                        'title': $scope.categoriesToInclude[i].title,
-                        'path': BASE_URL + '#/category/' + $scope.categoriesToInclude[i].slug
-                    });
-                }
+                $scope.menu.items = $scope.itemsToInclude;
+
                 crudService.updateMenu($scope.menu._id, $scope.menu).then(function (res) {
                     if (res.data) {
                         $rootScope.$broadcast(SUCCESS_EVENTS.success, 'Menu updated.');
@@ -58,7 +74,30 @@ angular.module('admin.menus-edit', [
                     }
                 });
             };
-        });
+
+            function markObjectsAlreadyInMenu() {
+                for (var i = 0; i < $scope.itemsToInclude.length; i++) {
+                    var j;
+                    for (j = 0; j < $scope.pageItems.length; j++) {
+                        if (checkEquality($scope.itemsToInclude[i], $scope.pageItems[j])) {
+                            $scope.itemsToInclude[i] = $scope.pageItems[j];
+                        }
+                    }
+
+                    for (j = 0; j < $scope.categoryItems.length; j++) {
+                        if (checkEquality($scope.itemsToInclude[i], $scope.categoryItems[j])) {
+                            $scope.itemsToInclude[i] = $scope.categoryItems[j];
+                        }
+                    }
+                }
+            }
+
+            function checkEquality(item1, item2) {
+                return (item1.title === item2.title && item1.path === item2.path)
+            }
+        }
+    )
+;
 
 
 
