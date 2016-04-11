6f32bf18443913ab3f3ff4b402e2bbb9bdf1f787
mueller-jan <mueller.jan.87@gmail.com>
2016-04-11 18:19:14 +0200

    style change

--- frontend/src/app/public/main.js
+++ frontend/src/app/public/main.js
@@ -20,12 +20,11 @@ angular.module('app.main', [
     })
 
     .controller('MainCtrl',
-        function MainController($scope, $state, $filter, $location, crudService) {
+        function MainController($scope, $state, $filter, crudService) {
             crudService.getLayout().then(function(res) {
                $scope.layout = res.data;
                 var toolbar =  $filter('filter')(res.data, {name: 'toolbar'})[0];
                 $scope.menu = toolbar.items[0].items;
-                // $location.path($scope.menu[0].path)
             });
         });
 
